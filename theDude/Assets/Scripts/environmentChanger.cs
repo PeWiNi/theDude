8d5e519484e9e6668eaec6dcd3786a334f241e15
cristinaNiculescu <crissa_alessa@yahoo.com>
2016-01-30 00:59:18 +0100

    started on env changer

---
+++ theDude/Assets/Scripts/environmentChanger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class environmentChanger : MonoBehaviour {
+
+	public bool raining=false;
+	public float duration=20f;
+	public float durationMultiplier=1f;
+	public GameObject rainDrop;
+	public GameObject flame; 
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (fillTheScreen ());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator fillTheScreen(){
+		for (int i = 0; i < duration*durationMultiplier; i++) { 
+			StartCoroutine (addWave ());
+		}
+
+		yield return new WaitForSeconds (duration * durationMultiplier);
+		raining = !raining;
+	}
+
+	IEnumerator addWave(){
+	
+	}
+}
