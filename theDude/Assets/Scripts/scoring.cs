cd4ddffcc93c59262935a1262907a0a2b8e2f961
cristinaNiculescu <crissa_alessa@yahoo.com>
2016-01-30 21:56:59 +0100

    fixed scoring

--- theDude/Assets/Scripts/scoring.cs
+++ theDude/Assets/Scripts/scoring.cs
@@ -7,22 +7,29 @@ public class scoring : MonoBehaviour {
 	float pointMultiplier;
 	public static int score;
 	public Text text;
-
+	//GameObject hero;
+	Image img;
 
 	void Awake(){
 		startTime = Time.realtimeSinceStartup;
+		//hero = GameObject.Find ("hero");
 	}
 	// Use this for initialization
 	void Start () {
 		text = GameObject.Find ("score").GetComponent<Text> ();
+		score = 0;
+		img = GameObject.Find ("Panel").GetComponent<Image> ();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-		pointMultiplier = Time.realtimeSinceStartup - startTime/600f;
-		score = (int)((float)score + 3.5123f * pointMultiplier);
-		text.text = score.ToString ();
+		
+		if (!img.enabled) {
+			pointMultiplier = Time.realtimeSinceStartup - startTime / 1000f;
+			score = (int)((float)score + 0.2123f * pointMultiplier);
+			text.text = score.ToString ();
+		}
+
 	}
 }
