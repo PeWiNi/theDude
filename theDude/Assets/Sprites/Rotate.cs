9929b816ac6a1de2a6c724c24738efe439d40a7f
Tornady <clbw@itu.dk>
2016-01-30 18:21:59 +0100

    Bendy Bendy Legs :dancers:

--- theDude/Assets/Sprites/Rotate.cs
+++ theDude/Assets/Sprites/Rotate.cs
@@ -4,24 +4,32 @@ using System.Collections;
 public class Rotate : MonoBehaviour {
     Transform RightArm; // IJKL
     Transform LeftArm; // WASD
+    Transform RightLeg; // IJKL
+    Transform LeftLeg; // WASD
     float doomTimer;
     float nextActionTime = 10;
+    float time;
 
     // Use this for initialization
     void Start () {
         RightArm = GameObject.Find("RightArm").transform;
         LeftArm = GameObject.Find("LeftArm").transform;
+        RightLeg = GameObject.Find("RightLeg").transform;
+        LeftLeg = GameObject.Find("LeftLeg").transform;
         Reset();
         doomTimer = 1;
     }
 	
 	// Update is called once per frame
 	void Update () {
-	    if(Input.GetKeyDown(KeyCode.R)) {
+        time = Time.deltaTime * doomTimer;
+        if (Input.GetKeyDown(KeyCode.R)) {
             Reset();
         }
         RotationInputManager(RightArm, false);
         RotationInputManager(LeftArm, true);
+        BendKnees(RightLeg, false);
+        BendKnees(LeftLeg, true);
 
         if (Time.time > nextActionTime) {
             nextActionTime += 10; doomTimer += .1f;
@@ -71,7 +79,6 @@ public class Rotate : MonoBehaviour {
     }
 
     void RotationInputManager(Transform Arm, bool WASD) {
-        float time = Time.deltaTime * doomTimer;
         Vector3 rot = new Vector3();
         if ((WASD ? Input.GetAxis("Vertical") : Input.GetAxis("VerticalR")) > 0) {//.GetKey(WASD ? KeyCode.W : KeyCode.I)) {
             rot = new Vector3(0, 0, 180);
@@ -112,10 +119,35 @@ public class Rotate : MonoBehaviour {
                 0, 0, Mathf.Lerp(Arm.GetChild(0).transform.localRotation.eulerAngles.z, WASD ? 540 : -180, time / 3));
     }
 
+    void BendKnees(Transform Leg, bool left) {
+        if (Input.GetButton(left ? "moveLeftLeg" : "moveRightLeg")) { //QE
+            Leg.rotation = Quaternion.Euler(Vector3.Lerp(Leg.rotation.eulerAngles,
+                new Vector3(0, 0, 90), time * 2));
+            int bendy;
+            if (Leg.rotation.eulerAngles.z < 85)
+                bendy = 270;
+            else
+                bendy = 359;
+            Leg.GetChild(0).transform.localRotation = Quaternion.Euler(
+                0, 0, Mathf.Lerp(Leg.GetChild(0).transform.localRotation.eulerAngles.z, bendy, time));
+        }
+        if (Input.GetButton(left ? "left" : "right")) { //ZC
+            Leg.rotation = Quaternion.Euler(Vector3.Lerp(Leg.rotation.eulerAngles,
+                new Vector3(0, 0, 0), time * 2));
+            Leg.GetChild(0).transform.localRotation = Quaternion.Euler(
+                0, 0, Mathf.Lerp(Leg.GetChild(0).transform.localRotation.eulerAngles.z, 359, time));
+        }
+    }
+
     void Reset() {
         RightArm.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
         RightArm.GetChild(0).transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
         LeftArm.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
         LeftArm.GetChild(0).transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+
+        RightLeg.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        RightLeg.GetChild(0).transform.rotation = Quaternion.Euler(new Vector3(0, 0, 359));
+        LeftLeg.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        LeftLeg.GetChild(0).transform.rotation = Quaternion.Euler(new Vector3(0, 0, 359));
     }
 }
