fa0d4199fdef4a3f7318c5e34e3d16d0baa2f3ed
cristinaNiculescu <crissa_alessa@yahoo.com>
2016-01-30 02:39:04 +0100

    removed some useless crap
    added some ui - for which i needed the circle
    &
    working on env changer

--- theDude/Assets/Scripts/environmentChanger.cs
+++ theDude/Assets/Scripts/environmentChanger.cs
@@ -7,11 +7,16 @@ public class environmentChanger : MonoBehaviour {
 	public float duration=20f;
 	public float durationMultiplier=1f;
 	public GameObject rainDrop;
-	public GameObject flame; 
+	public GameObject flame1; 
+	public GameObject flame2; 
+	public float time;
+	public float maXtime=300f;
+
 
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (fillTheScreen ());
+		time = Time.realtimeSinceStartup;
 
 	}
 	
@@ -27,9 +32,31 @@ public class environmentChanger : MonoBehaviour {
 
 		yield return new WaitForSeconds (duration * durationMultiplier);
 		raining = !raining;
+	
+		float currentTime = Time.realtimeSinceStartup - time;
+		Debug.Log ("Current time :" + currentTime + " duration :" + duration * durationMultiplier + " raining: " + raining); 
+		if (currentTime * 3f < 300f)
+			durationMultiplier *= currentTime * 3f / 200f;
+
+		StartCoroutine (fillTheScreen ());
 	}
 
 	IEnumerator addWave(){
-	
+		yield return new WaitForSeconds (1f);
+
+		if (raining) 
+		{
+			Vector3 pos = new Vector3 (Random.Range (-9.5f,9.5f ), 11, Random.Range(0.2f,0.5f));
+			Instantiate (rainDrop, pos, Quaternion.identity);
+		} 
+		else 
+		{
+			int fl = (int)Random.Range (0, 100);
+			Vector3 pos = new Vector3 (Random.Range (-9.5f,9.5f ), 0, Random.Range(0.2f,0.5f));
+			if (fl <= 50)
+				Instantiate (flame1, pos, Quaternion.identity);
+			else
+				Instantiate (flame2, pos, Quaternion.identity);			
+		}
 	}
 }
