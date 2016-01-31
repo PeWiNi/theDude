83c7323e9c99279f58e00ae46836c654b1493334
Henrik Pedersen <henrikpedersen1@hotmail.com>
2016-01-31 06:41:14 +0100

    ARGH Fire & Rain!

--- theDude/Assets/Scripts/environmentChanger.cs
+++ theDude/Assets/Scripts/environmentChanger.cs
@@ -1,75 +1,89 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class environmentChanger : MonoBehaviour {
+public class environmentChanger : MonoBehaviour
+{
+    public GameObject camera_FIRE;
+    public GameObject camera_RAIN;
+
+    public bool raining = false;
+    public float duration = 2f;
+    public float durationMultiplier = 1f;
+    public GameObject rainDrop;
+    public GameObject rainDrop2;
+    public GameObject flame1;
+    public GameObject flame2;
+    public float time;
 
 
-	public bool raining=false;
-	public float duration=2f;
-	public float durationMultiplier=1f;
-	public GameObject rainDrop;
-	public GameObject rainDrop2;
-	public GameObject flame1; 
-	public GameObject flame2; 
-	public float time;
+    public static int noFlames = 0;
 
+    public static int noDrops = 0;
 
-	public static int noFlames=0;
+    // Use this for initialization
+    void Start()
+    {
+        time = Time.realtimeSinceStartup;
+        StartCoroutine(addWave(10f, true));
+    }
 
-	public static int noDrops=0;
+    // Update is called once per frame
+    void Update()
+    {
 
-	// Use this for initialization
-	void Start () {
-		time = Time.realtimeSinceStartup;
-		StartCoroutine(addWave(10f,true));
-	}
+        Debug.Log("raindrops : " + noDrops + " flames: " + noFlames);
+    }
 
-	// Update is called once per frame
-	void Update () {
-	
-		Debug.Log ("raindrops : " + noDrops + " flames: " + noFlames); 
-	}
+    public IEnumerator fillTheScreen()
+    {
+        float currentTime = Time.realtimeSinceStartup - time;
 
-	public IEnumerator fillTheScreen(){
-		float currentTime = Time.realtimeSinceStartup - time;
+        durationMultiplier = durationMultiplier * currentTime / 600f + durationMultiplier;
+        StartCoroutine(addWave(duration * durationMultiplier, true));
+        yield return new WaitForSeconds(duration * durationMultiplier);
+    }
 
-		durationMultiplier = durationMultiplier * currentTime / 600f + durationMultiplier;
-		StartCoroutine(addWave (duration * durationMultiplier, true));
-		yield return new WaitForSeconds (duration * durationMultiplier);
-	}
+    public IEnumerator addWave(float progress, bool goOn)
+    {
+        while (goOn)
+        {
+            yield return new WaitForSeconds(0.001f);
+            progress -= 0.001f;
+            if (progress < 0f)
+                goOn = false;
+            if (raining)
+            {
+                if (!camera_RAIN.GetComponent<AudioSource>().isPlaying)
+                    camera_RAIN.GetComponent<AudioSource>().Play();
 
-	public IEnumerator addWave(float progress, bool goOn){
-		while(goOn)
-		{
-		yield return new WaitForSeconds (0.001f);
-			progress -= 0.001f;
-			if (progress < 0f)
-				goOn = false;
-			if (raining) {
-				int rd = (int)Random.Range (0, 100);
-				Vector3 pos = new Vector3 (Random.Range (-23f, 23f), 18.5f, Random.Range (0.02f, 0.03f));
-				noDrops++;
-				if (rd <= 50)
-					Instantiate (rainDrop, pos, Quaternion.identity);
-				else
-					Instantiate (rainDrop2, pos, Quaternion.identity);
-				
+                int rd = (int)Random.Range(0, 100);
+                Vector3 pos = new Vector3(Random.Range(-23f, 23f), 18.5f, Random.Range(0.02f, 0.03f));
+                noDrops++;
+                if (rd <= 50)
+                    Instantiate(rainDrop, pos, Quaternion.identity);
+                else
+                    Instantiate(rainDrop2, pos, Quaternion.identity);
+
                 //Kill Clouds
-				//foreach (CloudScript cs in GameObject.FindObjectsOfType<CloudScript>())
-				//	cs.KillMe ();
-			} 
-			else {
-				noFlames++;
-				//Debug.Log (noFlames);
-				int fl = (int)Random.Range (0, 100);
-				Vector3 pos = new Vector3 (Random.Range(0,2) < 1 ? Random.Range (-24.5f,-1f) : Random.Range(1f, 24.5f), -0.8f, Random.Range (0.02f, 0.03f));
-				if (fl <= 50)
-					Instantiate (flame1, pos, Quaternion.identity);
-				else
-					Instantiate (flame2, pos, Quaternion.identity);	
-				
-			}
-		}
-		}
+                //foreach (CloudScript cs in GameObject.FindObjectsOfType<CloudScript>())
+                //	cs.KillMe ();
+            }
+            else
+            {
+                if (!camera_FIRE.GetComponent<AudioSource>().isPlaying)
+                    camera_FIRE.GetComponent<AudioSource>().Play();
+
+                noFlames++;
+                //Debug.Log (noFlames);
+                int fl = (int)Random.Range(0, 100);
+                Vector3 pos = new Vector3(Random.Range(0, 2) < 1 ? Random.Range(-24.5f, -1f) : Random.Range(1f, 24.5f), -0.8f, Random.Range(0.02f, 0.03f));
+                if (fl <= 50)
+                    Instantiate(flame1, pos, Quaternion.identity);
+                else
+                    Instantiate(flame2, pos, Quaternion.identity);
+
+            }
+        }
+    }
 }
 
--- theDude/Assets/Scripts/masterWave.cs
+++ theDude/Assets/Scripts/masterWave.cs
@@ -2,73 +2,90 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class masterWave : MonoBehaviour {
+public class masterWave : MonoBehaviour
+{
 
-	Vector3 enlarge=new Vector3(1f,1f,1f);
-	Vector3 shrink=new Vector3(1f,1f,1f);
-	public GameObject lavaWaves;	
-	GameObject panel;
-	Image img;
-	public environmentChanger env;
-	//waves[] wvs;
+    Vector3 enlarge = new Vector3(1f, 1f, 1f);
+    Vector3 shrink = new Vector3(1f, 1f, 1f);
+    public GameObject lavaWaves;
+    GameObject panel;
+    Image img;
+    public environmentChanger env;
+    //waves[] wvs;
 
-	// Use this for initialization
-	void Awake()
-	{
-		lavaWaves = GameObject.Find("lavaWaves");
-		env = GameObject.Find ("envChanger").GetComponent<environmentChanger> ();
-		//wvs = gameObject.GetComponents<waves> ();
-	}
+    public GameObject camera_FIRE;
+    public GameObject camera_RAIN;
 
-	void Start () 
-	{
-		panel= GameObject.Find ("Panel");
-		img= panel.GetComponent<Image> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Awake()
+    {
+        lavaWaves = GameObject.Find("lavaWaves");
+        env = GameObject.Find("envChanger").GetComponent<environmentChanger>();
+        //wvs = gameObject.GetComponents<waves> ();
+    }
+
+    void Start()
+    {
+        panel = GameObject.Find("Panel");
+        img = panel.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
 
-		if(env.raining) 
-		{	
-			//shrinking the lava wave
-			if (lavaWaves.transform.localScale.y > 0.01f )
-			{	lavaWaves.transform.localScale = new Vector3 (lavaWaves.transform.localScale.x, 
-				lavaWaves.transform.localScale.y-environmentChanger.noDrops / 1000000f,
-				lavaWaves.transform.localScale.z);
-			}
-			else
-			{
-				//enlarge the water wave
-				enlarge = new Vector3 (transform.localScale.x,
-					transform.localScale.y + environmentChanger.noDrops / 1000000f,
-					transform.localScale.z);
-				gameObject.transform.localScale = enlarge;
-				Vector3 newPos = new Vector3 (gameObject.transform.position.x, 
-					                 gameObject.transform.position.y ,
-				//	-1.55f, 
-					gameObject.transform.position.z);
-				gameObject.transform.position = newPos;
-			}
-		}
-	else
-			//shrink the water wave
-		if (environmentChanger.noFlames >= 0 && transform.localScale.y > 0.01f) 
-		{
-		shrink = new Vector3 (transform.localScale.x,
-				transform.localScale.y - environmentChanger.noFlames / 1000000f,
-				transform.localScale.z);
-			gameObject.transform.localScale = shrink;
-		Vector3 newPos = new Vector3 (gameObject.transform.position.x, 
-				gameObject.transform.position.y, //- environmentChanger.noFlames / 100000f,
-				//	-1.55f,
-				gameObject.transform.position.z);
-			gameObject.transform.position = newPos;
-		}
-						
-	
-		if (img.enabled == true)
-			Destroy (gameObject);		
-	}
+        if (env.raining)
+        {
+            //shrinking the lava wave
+            if (lavaWaves.transform.localScale.y > 0.01f)
+            {
+                if (camera_FIRE.GetComponent<AudioSource>().volume > 0f && camera_FIRE.GetComponent<AudioSource>().isPlaying)
+                    camera_FIRE.GetComponent<AudioSource>().volume -= 0.01f;
+                else if (camera_FIRE.GetComponent<AudioSource>().volume == 0f)
+                {
+                    camera_FIRE.GetComponent<AudioSource>().Stop();
+                    camera_FIRE.GetComponent<AudioSource>().volume = 1f;
+                }
+
+                lavaWaves.transform.localScale = new Vector3(lavaWaves.transform.localScale.x,
+                lavaWaves.transform.localScale.y - environmentChanger.noDrops / 1000000f,
+                lavaWaves.transform.localScale.z);
+            }
+            else
+            {
+                //enlarge the water wave
+                enlarge = new Vector3(transform.localScale.x,
+                    transform.localScale.y + environmentChanger.noDrops / 1000000f,
+                    transform.localScale.z);
+                gameObject.transform.localScale = enlarge;
+                Vector3 newPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+                gameObject.transform.position = newPos;
+            }
+        }
+        else
+            //shrink the water wave
+            if (environmentChanger.noFlames >= 0 && transform.localScale.y > 0.01f)
+            {
+                if (camera_RAIN.GetComponent<AudioSource>().volume > 0f && camera_RAIN.GetComponent<AudioSource>().isPlaying)
+                    camera_RAIN.GetComponent<AudioSource>().volume -= 0.01f;
+                else if (camera_RAIN.GetComponent<AudioSource>().volume == 0f)
+                {
+                    camera_RAIN.GetComponent<AudioSource>().Stop();
+                    camera_RAIN.GetComponent<AudioSource>().volume = 1f;
+                }
+                
+                shrink = new Vector3(transform.localScale.x,
+                        transform.localScale.y - environmentChanger.noFlames / 1000000f,
+                        transform.localScale.z);
+                gameObject.transform.localScale = shrink;
+                Vector3 newPos = new Vector3(gameObject.transform.position.x,
+                        gameObject.transform.position.y, //- environmentChanger.noFlames / 100000f,
+                    //	-1.55f,
+                        gameObject.transform.position.z);
+                gameObject.transform.position = newPos;
+            }
+        if (img.enabled == true)
+            Destroy(gameObject);
+    }
 }
